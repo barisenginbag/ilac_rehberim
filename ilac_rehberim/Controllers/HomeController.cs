a21b2944051afbe628d6a2cc167e3074648ded81
Barış Enginbağ <113132649+barisenginbag@users.noreply.github.com>
2024-02-22 01:22:30 +0300

    Add project files.

---
+++ ilac_rehberim/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ilac_rehberim.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ilac_rehberim.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+		public IActionResult Kayit()
+		{
+			return View();
+		}
+		public IActionResult Giris()
+		{
+			return View();
+		}
+		public IActionResult Buy()
+		{
+			return View();
+		}
+		public IActionResult Index1()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ilac_rehberim/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ilac_rehberim.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
